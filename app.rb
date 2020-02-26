868b5bbda3640ccea5e9079b493b88e95c812650
Harindrar Jayanthinathan <harindrar@u.yale-nus.edu.sg>
2020-02-26 08:07:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Harindrar"
+age = 22
+favorite_food = "Chicken Rice"
+height_in_inches = 
 
 ## Do not modify below this line!
 
